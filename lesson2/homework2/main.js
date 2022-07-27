974959d6f3996956abcb851b0a5d56c3d05ee59e
karyyyna <nagkarina2006@gmail.com>
2022-07-27 15:00:19 +0200

    Initial commit

---
+++ lesson2/homework2/main.js
@@ -0,0 +1,86 @@
+
+let u1 = 'Karina';
+let u2 = 'Olya';
+let u3 = 'Masha';
+let u4 = 'Katya';
+let u5 = 'Lisa';
+let u6 = 'Margo';
+let u7 = 'Alla';
+let u8 = 'Tanya';
+let u9 = 'Anna';
+let u10 = 'Dana';
+   let girle = [ u1, u2, u3, u4, u5, u6, u7, u8, u9, u10 ]
+   console.log(girle)
+
+
+let book1 = {
+    title: `Bible`,
+    pageCount: 1800,
+    genre: 'prophecies'
+    };
+console.log(book1)
+let book2 = {
+    title: `The Da Vinci Code`,
+    pageCount: 637,
+    genre: 'novel'
+};
+console.log(book2)
+let book3 = {
+    title: `Gone with the wind`,
+    pageCount: 932,
+    genre: 'novel'
+};
+console.log(book3)
+
+let name1 = {
+    title: `Bible`,
+    pageCount: 1800,
+    genre: 'prophecies',
+    authors: [
+        {
+            name: 'God',
+            age: 1000000000
+        }
+    ]
+
+};
+let name2 = {
+    title: `The Da Vinci Code`,
+    pageCount: 637,
+    genre: 'novel',
+    authors: [
+        {
+            name: 'Tomash',
+            age: 29
+        }
+    ]
+
+};
+let name3 = {
+    title: `Gone with the wind`,
+    pageCount: 932,
+    genre: 'novel',
+    authors: [
+        {
+            name: 'Sasha',
+            age: 34
+        }
+    ]
+};
+let books = [name1, name2, name3]
+console.log(books)
+
+let array = [
+    {name: 'Igor', username: 'jajaja', password: '938240'},
+    {name: 'Oleh', username: 'kakaka', password: '928930'},
+    {name: 'Marat', username: 'ldskdls', password: '0923984'},
+    {name: 'Pasha', username: 'ucvyvuv', password: '9873284'},
+    {name: 'Illya', username: 'dsnudi', password: '8638276'},
+    {name: 'Pavlo', username: 'isisisi', password: '982383'},
+    {name: 'Vitya', username: 'osoadosp', password: '626262'},
+    {name: 'Roma', username: 'txsrtxr', password: '087477'},
+    {name: 'Vlad', username: 'yuxtytx', password: '7637273'},
+    {name: 'Losha', username: 'ijdsjjo', password: '515151'},
+]
+let users =[array]
+console.log(users,['password'])
