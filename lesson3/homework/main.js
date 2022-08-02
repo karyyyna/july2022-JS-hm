98672bc88779bae18d0a0c741288b2adb9918711
karyyyna <nagkarina2006@gmail.com>
2022-08-02 13:22:13 +0200

    homework

---
+++ lesson3/homework/main.js
@@ -0,0 +1,141 @@
+// Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+// let number = prompt('number?');
+// switch (number) {
+//     case '1':
+//         console.log('done');
+//         break;
+//     case '0':
+//         console.log('not done');
+//         break;
+//     case '-3':
+//         console.log('done');
+//         break;
+// }
+//
+
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+// let time = +prompt('time');
+// if (time >= 0 && time <= 15){
+//     console.log('the first stroke');
+// }if (time >= 16 && time <= 30){
+//     console.log('the second stroke');
+// }if (time >= 31 && time <= 45){
+//     console.log('the third stroke');
+// }if (time >= 46 && time <= 59){
+//     console.log('the fourth stroke');
+// }
+
+
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+//
+
+
+
+
+// let day = prompt('day')
+// if (day >= 1 && day <= 10) {
+//     console.log('first dekada')
+// }if (day >= 11 && day <= 20) {
+//     console.log('second dekada')
+// }if (day >= 21 && day <= 31) {
+//     console.log('third dekada')
+// }
+
+
+
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+// let week = prompt('plan on week:');
+// switch (week) {
+//     case 'monday':
+//         console.log('eat');
+//         break;
+//     case 'tuesday':
+//         console.log('sleep');
+//         break;
+//     case 'wednesday':
+//         console.log('annoying');
+//         break;
+//     case 'thursday':
+//         console.log('die');
+//         break;
+//     case 'friday':
+//         console.log('job');
+//         break;
+//     case 'saturday':
+//         console.log('watch movie');
+//         break;
+//     case 'sunday':
+//         console.log('vacation');
+//         break;
+//     default:
+//         console.log('???')
+// }
+
+
+
+
+
+
+
+
+//     - Користувач вводить або має два числа.
+//         Потрібно знайти та вивести максимальне число з тих двох .
+//         Також потрібно врахувати коли введені рівні числа.
+//
+// let x = ('1');
+// let y = ('2');
+// y = 80;
+// x = 20;
+// if (x > y ){
+//     console.log('max-number');
+// } if (y > x){
+//     console.log('min-number')
+// } if (x === y) {
+//     console.log('equality')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+        // за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподыбне, тобто кастується до false)
+// let x = NaN || 'default';
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
